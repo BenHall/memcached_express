2d4db8993c843efb8192dc2a23843d82902c3f3d
BenHall <Ben@Benhall.me.uk>
2011-12-12 05:20:25 +0000

    Output a cacheable time

--- routes.js
+++ routes.js
@@ -1,6 +1,15 @@
 exports.define = function(app) { 
-  app.get('/', function(req, res){
-      res.render('index', {title: "Hello Express"});
-  }); 
+  root(app);
 };
 
+function root(app) {
+  function microtime() {
+    var now = new Date().getTime() / 1000;
+    var s = parseInt(now);
+    return (Math.round((now - s) * 1000) / 1000) + ' ' + s;
+  }
+
+  app.get('/', function(req, res){
+      res.render('index', {title: "Hello Express at " + microtime()});
+  }); 
+};
