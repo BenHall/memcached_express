4dcb76b06a049adfe6a5d5b901e2eabbf4985a92
BenHall <Ben@Benhall.me.uk>
2011-12-12 05:41:44 +0000

    Cache output

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+var memcache = require('memcache');
+
 exports.define = function(app) { 
   root(app);
 };
@@ -9,7 +11,25 @@ function root(app) {
     return (Math.round((now - s) * 1000) / 1000) + ' ' + s;
   }
 
+  function cache(key, value) {
+    client.set(key, value, function(error, result){}, 10);
+  }
+
   app.get('/', function(req, res){
-      res.render('index', {title: "Hello Express at " + microtime()});
+    client.get('key', function(error, result){ 
+      console.log(result);
+
+      if(result == null)  {
+        result = microtime();
+        cache('key', result);
+      }
+
+      res.render('index', {title: "Hello Express at " + result});
+    });
   }); 
+
+
+  var client = new memcache.Client();
+  client.connect();
+  
 };
